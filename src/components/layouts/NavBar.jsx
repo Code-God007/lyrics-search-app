5db6204ef89cfaa543c3d07254f738e77b610845
Nitish Dhiman <dhimannitish0828@gmail.com>
2019-11-16 22:03:51 +0530

    initial commit

---
+++ src/components/layouts/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-dark bg-dark mb-5">
+      <span className="navbar-brand mb-0 display-3 mx-auto">Lyrics Finder</span>
+    </nav>
+  );
+};
+
+export default NavBar;
